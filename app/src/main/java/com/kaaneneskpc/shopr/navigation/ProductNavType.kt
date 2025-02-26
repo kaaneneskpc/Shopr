6d40ed4bf43f4176006ec75e120c5c33f7b629a8
kkapici <kaaneneskpc1@gmail.com>
2025-02-26 20:02:21 +0300

    Adding productNavType before O Build Version Code

--- app/src/main/java/com/kaaneneskpc/shopr/navigation/ProductNavType.kt
+++ app/src/main/java/com/kaaneneskpc/shopr/navigation/ProductNavType.kt
@@ -1,7 +1,6 @@
 package com.kaaneneskpc.shopr.navigation
 
 import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.core.bundle.Bundle
 import androidx.navigation.NavType
 import com.kaaneneskpc.shopr.model.UiProductModel
@@ -11,7 +10,6 @@ import java.net.URLDecoder
 import java.net.URLEncoder
 import java.util.Base64
 
-@RequiresApi(Build.VERSION_CODES.O)
 val productNavType = object : NavType<UiProductModel>(isNullableAllowed = false) {
     override fun get(bundle: Bundle, key: String): UiProductModel? {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
@@ -22,26 +20,60 @@ val productNavType = object : NavType<UiProductModel>(isNullableAllowed = false)
     override fun parseValue(value: String): UiProductModel {
         val item = Json.decodeFromString<UiProductModel>(value)
 
-        return item.copy(
-            image = URLDecoder.decode(item.image, "UTF-8"),
-            description = String(Base64.getDecoder().decode(item.description.replace("_", "/"))),
-            title = String(Base64.getDecoder().decode(item.title.replace("_", "/")))
-        )
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            item.copy(
+                image = URLDecoder.decode(item.image, "UTF-8"),
+                description = String(
+                    Base64.getDecoder().decode(item.description.replace("_", "/"))
+                ),
+                title = String(Base64.getDecoder().decode(item.title.replace("_", "/")))
+            )
+        } else {
+            item.copy(
+                image = URLDecoder.decode(item.image, "UTF-8"),
+                description = String(
+                    android.util.Base64.decode(
+                        item.description.replace("_", "/"),
+                        android.util.Base64.DEFAULT
+                    )
+                ),
+                title = String(
+                    android.util.Base64.decode(
+                        item.title.replace("_", "/"),
+                        android.util.Base64.DEFAULT
+                    )
+                )
+            )
+        }
     }
 
     override fun serializeAsValue(value: UiProductModel): String {
-        return Json.encodeToString(
-            value.copy(
-                image = URLEncoder.encode(value.image, "UTF-8"),
-                description = String(
-                    Base64.getEncoder().encode(value.description.toByteArray())
-                ).replace("/", "_"),
-                title = String(Base64.getEncoder().encode(value.title.toByteArray())).replace(
-                    "/",
-                    "_"
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Json.encodeToString(
+                value.copy(
+                    image = URLEncoder.encode(value.image, "UTF-8"),
+                    description = String(
+                        Base64.getEncoder().encode(value.description.toByteArray())
+                    ).replace("/", "_"),
+                    title = String(Base64.getEncoder().encode(value.title.toByteArray())).replace(
+                        "/",
+                        "_"
+                    )
                 )
             )
-        )
+        } else {
+            Json.encodeToString(
+                value.copy(
+                    image = URLEncoder.encode(value.image, "UTF-8"),
+                    description = String(
+                        android.util.Base64.encode(value.description.toByteArray(), android.util.Base64.DEFAULT)
+                    ).replace("/", "_"),
+                    title = String(
+                        android.util.Base64.encode(value.title.toByteArray(), android.util.Base64.DEFAULT)
+                    ).replace("/", "_")
+                )
+            )
+        }
     }
 
     override fun put(bundle: Bundle, key: String, value: UiProductModel) {
